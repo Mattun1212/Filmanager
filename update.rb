74f574e310bcf763be05da0743bd3e9e410fe301
{Mattun1212} <{kouta.dvlp@icloud.com}>
2021-06-04 09:48:39 +0000

    update.rbをupdate

--- update.rb
+++ update.rb
@@ -1,7 +1,8 @@
 require 'csv'
 require './update_on_screen.rb'
 require './scraping_on_screen.rb'
-require 'bundler/setup'
-Bundler.require
+require 'sinatra/activerecord'
+require 'open-uri'
+require 'nokogiri'
 require './models.rb'
 Everyday.update_on_screen_data
