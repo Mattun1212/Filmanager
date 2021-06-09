8edbe6cd46660b333ab106d5656a771e4fcd2737
{Mattun1212} <{kouta.dvlp@icloud.com}>
2021-06-09 07:55:49 +0000

    日付判別

--- update.rb
+++ update.rb
@@ -5,4 +5,5 @@ require 'sinatra/activerecord'
 require 'open-uri'
 require 'nokogiri'
 require './models.rb'
+require 'date'
 Everyday.update_on_screen_data
--- update_on_screen.rb
+++ update_on_screen.rb
@@ -15,6 +15,20 @@ module Everyday
        Movie.find_by(title: movie[0], movie_id: movie[1], theater: theater.name).update(finish: movie[2])
       end
     end
+    today = Date.today
+    dates=Movie.all
+    dates.each do |date|
+     if date.finish.present?
+      finish=date.finish.split('/')
+      if today.month==finish[0]
+        if today.date > finish[1]
+         Subscription.find_by(movie_id: date.id).destroy
+        end
+      elsif today.month>finish[0]
+         Subscription.find_by(movie_id: date.id).destroy
+      end
+     end
+    end
     
     @movies.each do |movie|
         movie[3]=theater.name
