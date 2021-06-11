d61beeefd4b039c41d145fdc62fb24e2da2bcb62
{Mattun1212} <{kouta.dvlp@icloud.com}>
2021-06-11 11:39:25 +0000

    img

--- update_img.rb
+++ update_img.rb
@@ -1,16 +1,16 @@
  module Thumb
  def self.update_img
-   movies=Movie.all
-   movies.each do |movie|
-    m_url='https://www.unitedcinemas.jp/'+movie.theater+'/film.php?film='+movie.movie_id.to_s
-        begin
-        info = Scraping_movie.load_movie_data(m_url)
-        Movie.find_by(movie_id: movie.movie_id, theater: movie.theater).update(img: info[1].strip)
-        Today.find_by(movie_id: movie.movie_id, theater: theater.name).update(img: info[1].strip)
-        rescue => e
-          puts e
-        end
-    end
+#   movies=Movie.all
+#   movies.each do |movie|
+#     m_url='https://www.unitedcinemas.jp/'+movie.theater+'/film.php?film='+movie.movie_id.to_s
+#         begin
+#         info = Scraping_movie.load_movie_data(m_url)
+#         Movie.find_by(movie_id: movie.movie_id, theater: movie.theater).update(img: info[1].strip)
+#         Today.find_by(movie_id: movie.movie_id, theater: movie.theater).update(img: info[1].strip)
+#         rescue => e
+#           puts e
+#         end
+#     end
     
     todays = Today.all   
     todays.each do |today|
@@ -18,7 +18,7 @@
         begin
         info = Scraping_movie.load_movie_data(m_url)
         Movie.find_by(movie_id: today.movie_id, theater: today.theater).update(img: info[1].strip)
-        Today.find_by(movie_id: today.movie_id, theater: theater.name).update(img: info[1].strip)
+        Today.find_by(movie_id: today.movie_id, theater: today.theater).update(img: info[1].strip)
         rescue => e
           puts e
         end
