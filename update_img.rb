e43361adeb78a6e747e281b71bb17798badf1a65
{Mattun1212} <{kouta.dvlp@icloud.com}>
2021-06-11 11:34:56 +0000

    img

--- update_img.rb
+++ update_img.rb
@@ -2,7 +2,7 @@
  def self.update_img
    movies=Movie.all
    movies.each do |movie|
-    m_url='https://www.unitedcinemas.jp/'+movie.theater+'/film.php?film='+movie.movie_id
+    m_url='https://www.unitedcinemas.jp/'+movie.theater+'/film.php?film='+movie.movie_id.to_s
         begin
         info = Scraping_movie.load_movie_data(m_url)
         Movie.find_by(movie_id: movie.movie_id, theater: movie.theater).update(img: info[1].strip)
@@ -14,7 +14,7 @@
     
     todays = Today.all   
     todays.each do |today|
-    m_url='https://www.unitedcinemas.jp/'+today.theater+'/film.php?film='+today.movie_id
+    m_url='https://www.unitedcinemas.jp/'+today.theater+'/film.php?film='+today.movie_id.to_s
         begin
         info = Scraping_movie.load_movie_data(m_url)
         Movie.find_by(movie_id: today.movie_id, theater: today.theater).update(img: info[1].strip)
