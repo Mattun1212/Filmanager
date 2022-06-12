fbfb5a5f2da96d01595290313f4dc95654be1a58
Mattun1212 <kouta.dvlp@icloud.com>
2022-06-12 14:55:32 +0000

    fix: dateのformatエラー修正

--- app.rb
+++ app.rb
@@ -80,9 +80,9 @@ post '/callback' do
             subscriptions=User.find_by(line_id: userId).movies
             subscriptions.each do |subscription|
               if subscription.finish.present?
-                finish=subscription.finish.month+'/'+subscription.finish.day+'終了'
-                theater='('+ Theater.find_by(name: subscription.theater).official+')'
-                content = subscription.title.strip+theater+finish
+               # finish=subscription.finish.month+'/'+subscription.finish.day+'終了'
+              #  theater='('+ Theater.find_by(name: subscription.theater).official+')'
+                content = 'a'
                 replyes.push(content)
               end
             end
