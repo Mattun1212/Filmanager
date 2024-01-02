9c21fa0c33ff670d2ab099e61bdcfcab4199a174
EC2 Default User <ec2-user@ip-10-1-3-220.ap-northeast-1.compute.internal>
2024-01-02 06:02:15 +0000

    fix: 文字化けを解消・システム復旧

--- scraping_movie.rb
+++ scraping_movie.rb
@@ -1,19 +1,14 @@
+require 'kconv'
 module Scraping_movie
  def self.load_movie_data(url)
-   charset = nil
    redirect_url = Net::HTTP.get_response(URI.parse(url))['location']
-   html =  OpenURI.open_uri(redirect_url) do |f|
-     sleep(1)
-     charset = f.charset
-     f.read
-   end
-
-   
-   doc = Nokogiri::HTML.parse(html, nil, charset)
-      m_data = []
-      m_data.push(doc.css('.movieDetailInfoFilm p').text.strip)
-      m_data.push(doc.css('#movieImage img')[0][:src])
-    #   m_data.push(doc.css('.movieDetailInfoFilm iframe')[0][:src])
+   html = OpenURI.open_uri(redirect_url, "r:binary").read
+   html = html.sub(/^<!DOCTYPE html(.*)$/, '<!DOCTYPE html>')
+   doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
+   m_data = []
+   m_data.push(doc.css('.movieDetailInfoFilm p').text.strip)
+   m_data.push(doc.css('#movieImage img')[0][:src])
+   m_data.push(doc.css('.movieDetailInfoFilm iframe')[0][:src])
   return m_data
  end
 end
--- scraping_on_screen.rb
+++ scraping_on_screen.rb
@@ -1,24 +1,18 @@
+require 'kconv'
 module Scraping_on_screen
  def self.load_schedule_data(url)
-   charset = nil
-   
-   html =  OpenURI.open_uri(url) do |f|
-     sleep(2)
-     charset = f.charset
-     f.read
-   end
-
    data = []
-   
-   doc = Nokogiri::HTML.parse(html, nil, charset)
+   html = OpenURI.open_uri(url, "r:binary").read
+   html = html.sub(/^<!DOCTYPE html(.*)$/, '<!DOCTYPE html>')
+   doc = Nokogiri::HTML.parse(html.toutf8, nil, 'utf-8')
 
    doc.css('li.clearfix').each do |node|
       m_data = []
-      m_data.push(node.css('span.movieTitle a').text.strip)
+      m_data.push(node.css('span.movieTitle a').first.text.strip)
       url_before = node.css('span.movieTitle a')[0][:href]
       id = url_before.split("=")[1].split("?")[0]
       m_data.push(id)
-      if  node.css('p.endInfo').text != ''
+      if node.css('p.endInfo').text != ''
        m_data.push(node.css('p.endInfo').text.split("上映")[0])
       end
       data.push(m_data)
