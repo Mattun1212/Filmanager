74b2bc2b3a6d993e356ea2f98bf3013d7d28047d
Mattun1212 <s2331711@u.tsukuba.ac.jp>
2024-03-04 16:26:38 +0000

    fix: 定期実行をリファクタリング&デモ画像を再更新

--- update_img.rb
+++ update_img.rb
@@ -1,18 +1,25 @@
 module Thumb
- def self.update_img
-    todays = Today.all   
-    todays.each do |today|
-    m_url='https://www.unitedcinemas.jp/'+today.theater+'/film.php?film='+today.movie_id.to_s
-        begin
+  def self.update_img
+    Today.all.each do |today|
+      m_url = "https://www.unitedcinemas.jp/#{today.theater}/film.php?film=#{today.movie_id}"
+      begin
         info = Scraping_movie.load_movie_data(m_url)
         movie = Movie.find_by(movie_id: today.movie_id, theater: today.theater)
-        if movie.img == "no_img.png"
-            Movie.find_by(movie_id: today.movie_id, theater: today.theater).update(img: info[1].strip)
-        end
-        Today.find_by(movie_id: today.movie_id, theater: today.theater).update(img: info[1].strip)
-        rescue => e
-          puts e
-        end
+        next unless info[1].present? && info[1].strip != "no_img.png"
+        
+        movie.update(img: info[1].strip) if movie.img == "no_img.png" || !valid_image_url?(movie.img)
+        today.update(img: movie.img)
+      rescue => e
+        puts e
+      end
     end
- end
+  end
+
+  def self.valid_image_url?(url)
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.is_a?(Net::HTTPSuccess)
+  rescue
+    false
+  end
 end
